585de2428fe57c056659c7f7a1fac362072640b7
Gaurav Shukla <heygauravshukla@gmail.com>
2025-08-23 13:25:06 +0530

    feat: revamp projects list

--- src/components/projects-list.tsx
+++ src/components/projects-list.tsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
 import * as motion from "motion/react-client";
-import { ExternalLink } from "lucide-react";
+import { ArrowUpRight } from "lucide-react";
 
 import { projects } from "@/data/projects";
 
 export function ProjectsList({ limit }: { limit?: number }) {
   return (
-    <ul className="grid gap-y-6 sm:grid-cols-2 sm:gap-x-4 md:gap-x-6 md:gap-y-8 lg:grid-cols-3">
+    <div className="grid gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-3">
       {projects.slice(0, limit).map((project, idx) => {
         return (
-          <motion.li
+          <motion.article
             key={project.title}
             initial={{ opacity: 0, x: -20 }}
             whileInView={{ opacity: 1, x: 0 }}
@@ -19,48 +19,37 @@ export function ProjectsList({ limit }: { limit?: number }) {
               ease: "easeOut",
             }}
             viewport={{ once: true }}
-            className="group bg-card text-card-foreground relative isolate overflow-hidden rounded-2xl border"
+            className="group bg-card text-card-foreground relative isolate h-50 min-w-60 overflow-hidden rounded-2xl border focus-within:ring"
           >
-            <Image
-              src={project.image}
-              alt={project.title}
-              width={400}
-              height={300}
-              className="aspect-video w-full object-cover transition-all group-hover:scale-105"
-            />
-
-            <div className="p-4">
-              <h3 className="font-medium tracking-tight">
+            <div className="flex flex-col gap-1 px-6 py-5">
+              <h3 className="overflow-hidden font-medium tracking-tight text-ellipsis whitespace-nowrap">
                 <a
                   href={project.link}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="flex items-center gap-2"
+                  className="flex max-w-max items-center pr-2.5 focus-visible:outline-none"
                 >
-                  <span className="absolute inset-0"></span>
+                  <span className="absolute inset-0 z-10"></span>
                   {project.title}
-                  <ExternalLink className="size-4" />
+                  <ArrowUpRight className="size-4 origin-center -translate-x-0.5 opacity-0 transition-all duration-100 ease-out group-hover:translate-x-1 group-hover:opacity-100" />
                 </a>
               </h3>
 
-              <p className="text-muted-foreground mt-2 text-sm/normal">
+              <p className="text-muted-foreground line-clamp-2 text-sm/normal text-pretty">
                 {project.description}
               </p>
-
-              <ul className="mt-4 flex flex-wrap gap-2">
-                {project.stack.map((tech) => (
-                  <li
-                    key={tech}
-                    className="bg-primary/10 text-primary rounded-full px-3 py-1.5 text-xs font-medium"
-                  >
-                    {tech}
-                  </li>
-                ))}
-              </ul>
             </div>
-          </motion.li>
+
+            <Image
+              src={project.image}
+              alt={project.title}
+              width={300}
+              height={100}
+              className="absolute top-28 -right-10 -rotate-6 rounded-2xl border object-cover transition-transform duration-100 ease-out group-hover:translate-x-0.5 group-hover:-translate-y-1 group-hover:-rotate-3"
+            />
+          </motion.article>
         );
       })}
-    </ul>
+    </div>
   );
 }
