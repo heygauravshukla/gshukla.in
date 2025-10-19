125d20fbf411d2761751d7f3f7b431d20b3917b6
Gaurav Shukla <heygauravshukla@gmail.com>
2025-10-19 21:38:24 +0530

    refactor: reorganize imports and update PostPage layout for improved structure

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
+import Image from "next/image";
+import { ArrowLeft, Calendar } from "lucide-react";
 import { PortableText, type SanityDocument } from "next-sanity";
+
+import Layout from "@/components/layout";
+import { Button } from "@/components/ui/button";
 import imageUrlBuilder from "@sanity/image-url";
 import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
 import { client } from "@/sanity/lib/client";
-import Link from "next/link";
-import Image from "next/image";
-import Layout from "@/components/layout";
 
 const POST_QUERY = `*[_type == "post" && slug.current == $slug][0]`;
 
@@ -32,25 +35,42 @@ export default async function PostPage({
 
   return (
     <Layout>
-      <main className="container mx-auto flex min-h-screen max-w-3xl flex-col gap-4 p-8">
-        <Link href="/" className="hover:underline">
-          ← Back to posts
-        </Link>
-        {postImageUrl && (
-          <Image
-            src={postImageUrl}
-            alt={post.title}
-            className="aspect-video rounded-xl"
-            width="550"
-            height="310"
-          />
-        )}
-        <h1 className="mb-8 text-4xl font-bold">{post.title}</h1>
-        <div className="prose">
-          <p>Published: {new Date(post.publishedAt).toLocaleDateString()}</p>
+      <div className="container space-y-8 py-10 md:py-20">
+        <nav>
+          <Button asChild variant="outline" size="icon">
+            <Link href="/blog" aria-label="Back to projects">
+              <ArrowLeft />
+            </Link>
+          </Button>
+        </nav>
+
+        <article className="mx-auto max-w-[65ch]">
+          <time className="text-muted-foreground flex items-start gap-2 text-sm">
+            <Calendar className="h-[1lh] w-4" />
+            {new Date(post.publishedAt).toLocaleDateString("en-US", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            })}
+          </time>
+          <h1 className="mt-6 text-4xl/snug font-medium tracking-tight">
+            {post.title}
+          </h1>
+          {/* <p className="text-muted-foreground mt-6 leading-7 text-pretty">
+            {metadata.summary}
+          </p> */}
+          {postImageUrl && (
+            <Image
+              src={postImageUrl}
+              alt={post.title}
+              className="my-8 aspect-video w-full rounded-2xl border object-cover"
+              width="550"
+              height="310"
+            />
+          )}
           {Array.isArray(post.body) && <PortableText value={post.body} />}
-        </div>
-      </main>
+        </article>
+      </div>
     </Layout>
   );
 }
