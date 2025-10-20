70cb093c543c5f0f69288693f4ad6892fcc77355
Gaurav Shukla <heygauravshukla@gmail.com>
2025-10-20 15:55:54 +0530

    refactor: include summary field in POSTS_QUERY for enhanced post details

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,7 +5,7 @@ import { PageIntro } from "@/components/page-intro";
 import { PostsList } from "@/components/posts-list";
 import { client } from "@/sanity/lib/client";
 
-const POSTS_QUERY = `*[ _type == "post" && defined(slug.current)]|order(publishedAt desc)[0...12]{_id, title, slug, publishedAt}`;
+const POSTS_QUERY = `*[ _type == "post" && defined(slug.current)]|order(publishedAt desc)[0...12]{_id, title, summary, slug, publishedAt}`;
 const options = { next: { revalidate: 3600 } };
 
 export const metadata: Metadata = {
