3afe0771a50c5398df707d7bd311f4aca079dac5
Gaurav Shukla <heygauravshukla@gmail.com>
2025-11-16 18:33:23 +0530

    feat: add timeline schema type and integrate into main schema

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,6 +1,7 @@
 import { type SchemaTypeDefinition } from "sanity";
 import { postType } from "./postType";
+import timeline from "./timeline";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [postType],
+  types: [postType, timeline],
 };
