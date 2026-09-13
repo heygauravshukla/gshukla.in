9a229a720c1fc2a088184b958a38ecda80b1300c
Gaurav Shukla <heygauravshukla@gmail.com>
2026-09-13 12:44:12 +0530

    feat(studio): initialize sanity studio

---
+++ apps/studio/sanity.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'gshukla',
+
+  projectId: '60os6ewn',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
