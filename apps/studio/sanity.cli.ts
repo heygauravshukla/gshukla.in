fef7bd813505416cc2d0ca62b00a341d4f5346c0
Gaurav Shukla <heygauravshukla@gmail.com>
2026-09-16 22:24:11 +0530

    chore(studio): configure deployment appId in sanity.cli.ts

--- apps/studio/sanity.cli.ts
+++ apps/studio/sanity.cli.ts
@@ -3,9 +3,10 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '60os6ewn',
-    dataset: 'production'
+    dataset: 'production',
   },
   deployment: {
+    appId: 'qi3qdkj1lm3w7v0jm9twyelr',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
